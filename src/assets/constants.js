f7c4ef4db39cc06318b866f5b16135058a779650
Davismeru <davismeru911@gmail.com>
2025-06-12 12:06:16 +0300

    update api endpoint uri

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,2 +1,2 @@
-// export const base_api_uri = "https://server.ballhub.online";
-export const base_api_uri = "http://localhost:3000";
+export const base_api_uri = "https://live-stream-server-7e4x.onrender.com";
+// export const base_api_uri = "http://localhost:3000";
